cded837b072c5de2d8f95bc83a2fb3f81e070d2c
ashizius <shishlov1989@yandex.ru>
2024-11-14 18:52:59 +0300

    release v0.3.0

--- bin/get-certificate.sh
+++ bin/get-certificate.sh
@@ -4,7 +4,7 @@ cd ..
 mkdir -p certbot/www
 mkdir -p certbot/conf
 sudo docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d $SITE_URL
-cd nginx/
+cd ./nginx/
 mv default.conf default_without-ssl.conf
 mv default_ssl.conf default.conf
 
